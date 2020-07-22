8f3e7e705298fea85311edd2c8a3eb7a442d0de8
Chris Bragg <cbragg9@yahoo.com>
2020-07-21 17:48:11 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 class App extends React.Component {
 
